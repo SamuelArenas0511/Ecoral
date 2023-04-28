a6ffcaf8cf60367bdad33eb2cbf3a2089ef7d25e
SamuelArenas0511 <123911857+SamuelArenas0511@users.noreply.github.com>
2023-04-27 20:40:56 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -27,6 +27,20 @@ window.addEventListener("scroll", () => {
     header.classList.toggle("shadow", window.scrollY > 0)
 })
 
+// Cambio de color busqueda
+
+
+document.getElementById("F1").addEventListener("click", function(){
+    document.getElementById("barra-busqueda").style.border="solid 2px #408fc4"
+})
+
+document.getElementById("F2").addEventListener("click", function(){
+    document.getElementById("barra-busqueda").style.border="solid 2px #17c28f"
+})
+
+document.getElementById("F3").addEventListener("click", function(){
+    document.getElementById("barra-busqueda").style.border="solid 2px #e69011"
+})
 
 
 
