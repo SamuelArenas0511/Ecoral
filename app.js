43b90d3faf6879daeafddd0106578c11903241bf
SamuelArenas0511 <123911857+SamuelArenas0511@users.noreply.github.com>
2023-05-19 01:18:26 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,53 @@
+window.addEventListener("load", function(){
+    window.location.href = "#";
+})
+
+// Animar
+
+var animado = document.querySelectorAll(".animado");
+
+function mostrarScroll(){
+    let scrollTop = document.documentElement.scrollTop;
+    for(var i=0; i<animado.length; i++){
+        let alturaAnimado = animado[i].offsetTop
+        if(alturaAnimado - 500 < scrollTop){
+            animado[i].style.opacity= 1;
+            if(i==0 || i==1 || i==4 || i==5 || i==8 || i==9 ){
+                animado[i].classList.add("animar");
+            }else if(i==2 || i==3 || i==6 || i==7 || i==10 || i==11 ){
+                animado[i].classList.add("animarL");
+            }
+            
+        }
+    }
+}
+    window.addEventListener('scroll',mostrarScroll)      
+
+
+   
+
+
+window.onscroll = function() {
+    var y = window.scrollY;
+    if (y<50){
+        for(i=0; i<animado.length; i++){
+            animado[i].style.opacity= 0;
+            if(i==0 || i==1 || i==4 || i==5 || i==8 || i==9 ){
+                animado[i].classList.remove("animar");
+            }else if(i==2 || i==3 || i==6 || i==7 || i==10 || i==11 ){
+                animado[i].classList.remove("animarL");
+            }
+        }
+        
+    }
+  };
+
+
 // filtro js
 
 $(document).ready(function () {
     $(".filtro-item").click(function(){
+
         const value= $(this).attr("data-filter");
         if(value == "all"){
             $(".post-caja").show("1000")
@@ -28,20 +74,79 @@ window.addEventListener("scroll", () => {
 })
 
 // Cambio de color busqueda
+var f = document.getElementById("F")
+var f1 = document.getElementById("F1")
+var f2 = document.getElementById("F2")
+var f3 = document.getElementById("F3")
+var f4 = document.getElementById("F4")
 
+f.addEventListener("click", function(){
+    document.getElementById("barra-busqueda").style.border="solid 2px #49bb62"
+    f.classList.toggle("animate__heartBeat");
+    
+})
 
-document.getElementById("F1").addEventListener("click", function(){
+f1.addEventListener("click", function(){
     document.getElementById("barra-busqueda").style.border="solid 2px #408fc4"
+    f1.classList.toggle("animate__heartBeat");
+    
 })
 
-document.getElementById("F2").addEventListener("click", function(){
+f2.addEventListener("click", function(){
     document.getElementById("barra-busqueda").style.border="solid 2px #17c28f"
+    f2.classList.toggle("animate__heartBeat");
 })
 
-document.getElementById("F3").addEventListener("click", function(){
+
+f3.addEventListener("click", function(){
     document.getElementById("barra-busqueda").style.border="solid 2px #e69011"
+    f3.classList.toggle("animate__heartBeat");
+    
+})
+
+
+f4.addEventListener("click", function(){
+    document.getElementById("barra-busqueda").style.border="solid 2px #118ae6"
+    f4.classList.toggle("animate__heartBeat");
 })
 
 
 
+// funcion cambiar fondo
+/*
+var fondo =document.getElementById("inicio");
+var numero = 1;
+var indentificador;
+
+function fondoCambiante(){
+    indentificador = setInterval(CambiarFondo, 3000)
+}
+
+function CambiarFondo (){
+    if (numero <= 6){
+        fondo.style.background="url(img/bg"+numero+".jpg)";
+        fondo.style.transition="background-image 2s ease";
+        fondo.style.backgroundSize="cover";
+       
+        numero++;
+    }else(
+        numero = 1
+    )
+}
+fondoCambiante()
+*/
+
+
+
+// animacion 
+
+var titulo = document.getElementById("titulo")
+
+titulo.addEventListener("mouseover", function(){
+    titulo.classList.toggle("animate__bounce");
+})
+
+titulo.addEventListener("click", function(){
+    titulo.classList.toggle("animate__hinge");
+})
 
