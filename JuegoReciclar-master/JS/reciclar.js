a69c1055b0299629cf6109df7a237949e13936e9
SamuelArenas0511 <123911857+SamuelArenas0511@users.noreply.github.com>
2023-05-19 01:35:27 -0500

    Update reciclar.js

--- JuegoReciclar-master/JS/reciclar.js
+++ JuegoReciclar-master/JS/reciclar.js
@@ -18,6 +18,6 @@ window.addEventListener("DOMContentLoaded", function() {
   "hideMethod": "fadeOut"
 }
 
-    reciclar = new ReciclarGame();
+    reciclar = new reciclarGame();
     reciclar.introducirNombre();
 })
